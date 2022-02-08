11a5b633ca6492edc80b1ae35151790cefd5762d
MimoGraphix <mimographix@gmail.com>
2022-02-08 21:45:06 +0100

    Class regeneration Cenerating and Parser parsing test.

---
+++ src/UBL/v21/Common/UnqualifiedDataTypes/RateType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Uctoplus\UblWrapper\UBL\v21\Common\UnqualifiedDataTypes;
+
+use Uctoplus\UblWrapper\UBL\Schema\BasicComponent;
+
+/**
+ * Class RateType
+ *
+ * @author Mário <mario@uctoplus.sk>
+ * @copyright uctoplus.sk, s.r.o.
+ * @package Uctoplus\UblWrapper\UBL\v21\Common\UnqualifiedDataTypes
+ */
+class RateType extends BasicComponent
+{
+    protected $type = "udt:RateType";
+
+}
