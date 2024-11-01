2417b67a7157238d8ad6f03666e2ef8f7e37059c
MimoGraphix <mimographix@gmail.com>
2024-11-01 13:27:33 +0100

    Update PHPUnit to version 9

--- src/UBL/v21/Common/AggregateComponents/DutyType.php
+++ src/UBL/v21/Common/AggregateComponents/DutyType.php
@@ -5,7 +5,7 @@ namespace Uctoplus\UblWrapper\UBL\v21\Common\AggregateComponents;
 use Uctoplus\UblWrapper\UBL\Schema\AggregateComponent;
 use Uctoplus\UblWrapper\UBL\v21\Common\BasicComponents\AmountType;
 use Uctoplus\UblWrapper\UBL\v21\Common\BasicComponents\DutyCodeType;
-use Uctoplus\UblWrapper\UBL\v21\Common\BasicComponents\DutyType;
+use Uctoplus\UblWrapper\UBL\v21\Common\BasicComponents\DutyType as BasicDutyType;
 
 /**
  * Class DutyType
@@ -26,7 +26,7 @@ class DutyType extends AggregateComponent
 {
     protected $casts = [
         "cbc:Amount" => AmountType::class,
-        "cbc:Duty" => DutyType::class,
+        "cbc:Duty" => BasicDutyType::class,
         "cbc:DutyCode" => DutyCodeType::class,
         "cac:TaxCategory" => TaxCategoryType::class,
     ];
